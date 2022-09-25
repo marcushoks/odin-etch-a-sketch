3bad0d16d7cc5fed0245955c4726cf76f357bd0e
marcushoks <marcushoks@gmail.com>
2022-09-25 16:36:19 +0800

    Add buttons to change the resolution of the grid
    Also removes css variables as they are not used anymore. Besides that,
    the algorithm of creating grid cells is now turned into a function as
    it is needed for the callback for the new buttons' event listeners.

--- script.ts
+++ script.ts
@@ -1,28 +1,47 @@
 const calculateCellSize = (gridSize: number, columns: number) => {
-  return gridSize / columns;
+  return `${gridSize / columns}px`;
 };
 
 const paintCell = (target: HTMLElement, color: string) => {
   target.style.backgroundColor = color;
 };
 
-const GRID_SIZE = 640;
-let gridColumns = 16;
-let cellSize = `${calculateCellSize(GRID_SIZE, gridColumns)}px`;
+const createCells = (grid: HTMLElement, columns: number, cellSize: string) => {
+  grid.style.gridTemplateColumns = `repeat(${columns}, ${cellSize})`;
+  const cells = [];
+  for (let i = 0; i < columns ** 2; i++) {
+    const cell = document.createElement("div");
+    cell.className = "grid-cell";
+    cell.style.height = cellSize;
+    cell.style.width = cellSize;
+    cell.addEventListener("mouseover", (e) => {
+      paintCell(e.target as HTMLElement, "red");
+    });
+    cells.push(cell);
+  }
+  grid?.replaceChildren(...cells);
+};
 
-const body = document.querySelector("body");
-body?.style.setProperty("--grid-columns", String(gridColumns));
-body?.style.setProperty("--cell-size", cellSize);
+const GRID_SIZE = 640;
+const GRID_COLUMNS = 16;
 
 const gridContainer = document.querySelector(
   ".grid-container"
 ) as HTMLDivElement;
 
-for (let i = 0; i < gridColumns ** 2; i++) {
-  const gridCell = document.createElement("div");
-  gridCell.className = "grid-cell";
-  gridCell.addEventListener("mouseover", (e) => {
-    paintCell(e.target as HTMLElement, "red");
+createCells(
+  gridContainer,
+  GRID_COLUMNS,
+  calculateCellSize(GRID_SIZE, GRID_COLUMNS)
+);
+
+const gridResoBtns = document.querySelectorAll(".grid-reso-btn");
+gridResoBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const btn = e.target as HTMLButtonElement;
+    const columns = parseInt(
+      btn.getAttribute("data-resolution") ?? String(GRID_COLUMNS)
+    );
+    createCells(gridContainer, columns, calculateCellSize(GRID_SIZE, columns));
   });
-  gridContainer?.appendChild(gridCell);
-}
+});
